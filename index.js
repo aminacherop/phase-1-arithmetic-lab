7ffad5fe23768eb3a3fdc2204db3dc1399945a92
aminacherop <cheropamina26@gmail.com>
2025-03-05 00:23:23 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply);
 
+let random = Math.floor(Math.random () * 11);
+
+let num3 = 24;
+let num4 = 20;
+let mod = num3 % num4;
+console.log(mod); 
+let max = Math.max(2,3,4,5,20);
+console.log(max);
 // Write your code here
